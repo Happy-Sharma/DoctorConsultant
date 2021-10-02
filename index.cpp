4cdf57bde963afbc859326774de0efd5cc9119d9
Tarun Kamboj <tarunkamboj711@gmail.com>
2021-10-02 12:27:28 +0530

    #1 Added basic architecture to take details for consultation.

--- index.cpp
+++ index.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
- 
-int main ()
-{
-    cout<<"Hello World";
-     
-    return 0;
+
+int main() {
+	string probDes, dateAndTime;
+	cout << "Please enter your problem here.";
+	cin >> probDes;
+	
+	cout << "Please provide time and date when you want consultantion.";
+	cin >> dateAndTime;
+	
+	cout << "Your request is under process, we will let you know."
+	
+	return 0;
 }
